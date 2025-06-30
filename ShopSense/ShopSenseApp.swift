86eebeacd7af260b24b31e7007f85f73f5146573
Nidhish R Nair <nidhishnair@rocketmail.com>
2025-06-29 21:26:23 -0500

    Initial Commit

---
+++ ShopSense/ShopSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSenseApp.swift
+//  ShopSense
+//
+//  Created by Nidhish Nair on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopSenseTests/ShopSenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSenseTests.swift
+//  ShopSenseTests
+//
+//  Created by Nidhish Nair on 6/29/25.
+//
+
+import Testing
+@testable import ShopSense
+
+struct ShopSenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
